b0fc06664cdd7b256c696bcd649a6cb7c9c5d7ad
isaquesv <166270471+isaquesv@users.noreply.github.com>
2024-10-14 09:04:46 -0300

    Update MeServlet.java
    Atualizando o código

--- aula4_Servlets/src/java/web/MeServlet.java
+++ aula4_Servlets/src/java/web/MeServlet.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package web;
 
 import java.io.IOException;
@@ -12,28 +8,21 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 // Possibilita a captura de dados atrelados a data
-import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 /** @author isaquesv */
 @WebServlet(name = "MeServlet", urlPatterns = {"/me.html"})
 public class MeServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
-        LocalDateTime dataAtual = LocalDateTime.now();
+        // Criando um objeto Date
+        Date data = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(data);
         // Capturando o ano atual
-        int anoAtual = dataAtual.getYear();
+        int anoAtual = calendar.get(Calendar.YEAR);
         
         String RA = "1290482312027";
         String nome = "ISAQUE SILVA VENANCIO";
@@ -57,43 +46,13 @@ public class MeServlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
